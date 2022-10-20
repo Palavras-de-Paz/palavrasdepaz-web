09b50b0ed14a4f8d5414d9ad49b0acba210cf79a
EnioSFilho <eniofilho76@gmail.com>
2022-10-20 01:50:02 -0300

    feat/header feito com bootstrap responsivo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,47 +1,49 @@
-import React from 'react'
+import { Button, Container, Nav, Navbar,  Offcanvas } from 'react-bootstrap';
 
-
-function Header() {
+function OffcanvasExample() {
   return (
     <>
-      <header className='header'>
-
-          <container className="images-header">
-
-          <div>
-            <img src="/images/logo.png" alt="" />
-          </div>
-
-          <div>
-
-          <img src="/images/palavras.png" alt="" />
-
-          </div>
-
-          </container>
-
-          <container className="buttons-header">
-            <ul>
-
-              <li><a href='#'>Quem Somos</a></li>
-              <li><a href='#'>O Programa</a></li>
-              <li><a href='#'>Voluntários</a></li>
-              <li><a href='#'>Doações</a></li>
-              <li><a href='#'>Parcerias</a></li>
-
-            </ul>
-
-
-          </container>
-
-
-
-
-
-      </header>
-
+      <container className="styles-header">
+        {['xl'].map((expand) => (
+          <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+            <Container fluid>
+              <container className="header">
+                <div>
+                  <img src="/images/logo.png" alt="logo-header" />
+                </div>
+                <div>
+                  <img src="/images/palavras.png" alt="texto-header" />
+                </div>
+              </container>
+              <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+              <Navbar.Offcanvas
+                id={`offcanvasNavbar-expand-${expand}`}
+                aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                placement="end"
+              >
+                <Offcanvas.Header closeButton>
+                  <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                    MENU
+                  </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  <Nav className="justify-content-end flex-grow-1 pe-3">
+                    <Nav.Link href="#action1">Quem Somos</Nav.Link>
+                    <Nav.Link href="#action2">O Programa</Nav.Link>
+                    <Nav.Link href="#action2">Voluntários</Nav.Link>
+                    <Nav.Link href="#action2">Doações</Nav.Link>
+                    <Nav.Link href="#action2">Parcerias</Nav.Link>
+                  </Nav>
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
+            </Container>
+          </Navbar>
+        ))}
+      </container>
     </>
-  )
+  );
 }
 
-export default Header
+export default OffcanvasExample;
+
+
