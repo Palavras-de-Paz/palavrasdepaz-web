8aba6b8bd07a2da681c9664f85d877f3caaae2e7
/A. SIMON <adryel@live.it>
2022-10-24 17:20:33 -0300

    feat: carousel slide

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -10,12 +10,17 @@ import "swiper/css/pagination";
 // import required modules
 import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 import Image from "next/image";
+import Carousel_1 from '../../../public/static/images/Carrossel_1.png'
+import Carousel_2 from '../../../public/static/images/Carrossel_2.png'
+import Carousel_3 from '../../../public/static/images/Carrossel_3.png'
 import Box from "../atoms/box/Box";
+import Typography from "../atoms/typography/Typography";
+import * as S from "./styled"
 
 function CarouselSlide() {
     return (
         <>
-            <Swiper
+            <Swiper style={{zIndex: 5}}
                 cssMode={true}
                 navigation={true}
                 pagination={true}
@@ -23,12 +28,32 @@ function CarouselSlide() {
                 keyboard={true}
                 modules={[Navigation, Pagination, Mousewheel, Keyboard]}
                 className="mySwiper"
+                loop={true}
             >
                 <SwiperSlide>
-                    <Box bg="black" height="350px"></Box>
+                    <S.SlideContainer>
+                        <Image layout="fixed" width="10000%" height="616px" src={Carousel_1} alt="" />
+                        <S.TextSlide>Projeto com <br/> Moradores de Rua</S.TextSlide>
+                    </S.SlideContainer>
+
                 </SwiperSlide>
-                <SwiperSlide><Box bg="black" height="350px"></Box></SwiperSlide>
-                <SwiperSlide><Box bg="black" height="350px"></Box></SwiperSlide>
+
+                <SwiperSlide>
+                    <Box justify="center" align="center" >
+                        <Image layout="fixed" width="10000%" height="616px" src={Carousel_3} alt="" />
+                        <S.TextSlide>Projeto nos <br/>
+                            Presídios</S.TextSlide>
+                    </Box>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <Box justify="center" align="center" >
+                        <Image objectFit="cover" layout="fixed" width="10000%" height="616px" src={Carousel_2} alt="" />
+                        <S.TextSlide>Projeto nas <br /> Escolas</S.TextSlide>
+                    </Box>
+                </SwiperSlide>
+
+
             </Swiper>
         </>
     );
