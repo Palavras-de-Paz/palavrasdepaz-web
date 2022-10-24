24fd0554ba07fdea8cc21d29cc4ac6420c1d8eb8
/A. SIMON <adryel@live.it>
2022-10-24 16:53:42 -0300

    feat: fixed button "contato"

---
+++ src/components/fixedbutton/FixedButton.jsx
@@ -0,0 +1,7 @@
+import * as S from "./styled";
+
+export default function FixedButton() {
+  return (
+    <S.FixedBtn>CONTATO</S.FixedBtn>
+  )
+}
--- src/templates/home/home.template.jsx
+++ src/templates/home/home.template.jsx
@@ -7,18 +7,23 @@ import Box from '../../components/atoms/box/Box'
 import Card from '../../components/card/Card'
 import Button from '../../components/button/button'
 import SwiperJs from '../../components/swiper/Swiper'
+import FixedButton from '../../components/fixedbutton/FixedButton'
 
 
 export default function HomeTemplate() {
   return (
     <>
       <Header />
+
       <CarouselSlide />
       <GreenBar />
+
       <Box justify="center" padding="32px">
         <Typography fontSize="48px" fontWeight="800" lineHeight="75.55px" color='black' text="Depoimentos" />
       </Box>
+
       <Card />
+
       <Box justify="center" padding="0 0 32px 0 " gap="48px">
         <Button href="" justify="center" width="200px" height="56px" color="black" text="Mais Depoimentos" />
         <Button href="" justify="center" width="200px" height="56px" color="black" text="Galeria de Fotos" />
@@ -28,13 +33,19 @@ export default function HomeTemplate() {
         <iframe width="900" height="618" src="https://www.youtube.com/embed/X3cphCj9YVc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </Box>
 
-      <Box bg="rgba(33, 170, 133, 1)" padding="32px" margin="32px 0" justify="center" align="center" direction="column" textAlign="center">
+      <Box style={{ zIndex: -2 }} bg="rgba(33, 170, 133, 1)" padding="32px" margin="32px 0" justify="center" align="center" direction="column" textAlign="center">
         <Typography width="88%" fontWeight="700" fontSize="36px" color="white" text="``Não é o mundo que precisa de paz, são as pessoas,
 quando as pessoas tiverem paz interior, o mundo estará em paz.´´"/>
-        <Typography fontWeight="700" fontSize="36px" color="white" text="Prem Rawat"/>
+        <Typography fontWeight="700" fontSize="36px" color="white" text="Prem Rawat" />
       </Box>
 
       <SwiperJs />
+
+      <Box align="center" justify="center" padding="32px">
+        <Button bg="rgba(33, 170, 133, 1)" color="white" href="" justify="center" width="200px" height="56px" text="Seja um parceiro" />
+      </Box>
+
+      <FixedButton />
       <Footer />
     </>
   )
