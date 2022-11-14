3113f7f403f2807cde5e124d4651b8c3e74b03e4
/A. SIMON <adryel@live.it>
2022-11-14 15:28:58 -0300

    feat: depoimentos link

--- src/templates/home/home.template.jsx
+++ src/templates/home/home.template.jsx
@@ -25,7 +25,7 @@ export default function HomeTemplate() {
       <Card />
 
       <Box justify="center" padding="0 0 32px 0 " gap="48px">
-        <Button href="" justify="center" width="200px" height="56px" color="black" text="Mais Depoimentos" />
+        <Button href="/depoimentos" justify="center" width="200px" height="56px" color="black" text="Mais Depoimentos" />
         <Button href="/galeria" justify="center" width="200px" height="56px" color="black" text="Galeria de Fotos" />
       </Box>
 
