c5e55fb0a6d36761b32677e58fd7204ec6e22743
EnioSFilho <eniofilho76@gmail.com>
2022-12-05 16:14:15 -0300

    refatorado

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -108,7 +108,7 @@ function Footer() {
             <p>Direitos rezervados à Palavra de Paz</p>
           </div>
           <div className='copyright-footer'>
-            <p><a href='/desenvolvedores'>©2022 Desenvolvedores</a></p>
+            <p><Link href='/desenvolvedores'>©2022 Desenvolvedores</Link></p>
           </div>
           <div>
             <p>CNPJ: 05.920.548/0001-73</p>
