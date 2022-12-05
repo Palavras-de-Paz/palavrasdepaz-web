2c9839cb421acda8beed3335d7afc7f5cf8bf0c6
EnioSFilho <eniofilho76@gmail.com>
2022-12-05 15:43:50 -0300

    footer concluido parcialmente

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,6 +5,7 @@ import Evento2 from "./bntInsta"
 import Evento3 from "./btnLink"
 import Evento4 from "./btnFace"
 import Evento5 from './bntWhats'
+import Link from 'next/link'
 
 
 
@@ -15,19 +16,19 @@ function Footer() {
         <div className='line-footer'></div>
         <div className='medias-footer'>
           <div>
-            <a href='https://www.youtube.com/channel/UCkmhiQTeUfdvKIY9NG9Navg' target="_blank"><Image id="you"onMouseOver={Evento1} src="/static/images/youtube.png" width="40px" height="40px" alt='youtube' /></a>
+            <a href='https://www.youtube.com/channel/UCkmhiQTeUfdvKIY9NG9Navg' target="_blank"><Image id="you" onMouseOver={Evento1} src="/static/images/youtube.png" width="40px" height="40px" alt='youtube' /></a>
           </div>
           <div>
-            <a href='https://www.instagram.com/palavrasdepaz.br/' target="_blank"><Image id="insta"onMouseOver={Evento2} src="/static/images/instagram.png" width="40px" height="40px" alt='instagram' /></a>
+            <a href='https://www.instagram.com/palavrasdepaz.br/' target="_blank"><Image id="insta" onMouseOver={Evento2} src="/static/images/instagram.png" width="40px" height="40px" alt='instagram' /></a>
           </div>
           <div>
-           <a href='https://www.linkedin.com/company/palavrasdepaz/mycompany/' target="_blank"> <Image id='link' onMouseOver={Evento3} src="/static/images/linkedin.png" width="40px" height="40px" alt='linkedin' /></a>
+            <a href='https://www.linkedin.com/company/palavrasdepaz/mycompany/' target="_blank"> <Image id='link' onMouseOver={Evento3} src="/static/images/linkedin.png" width="40px" height="40px" alt='linkedin' /></a>
           </div>
           <div>
-          <a href='https://www.facebook.com/palavrasdepaz.br' target="_blank"><Image id='face' onMouseOver={Evento4} src="/static/images/facebook.png" width="40px" height="40px" alt='facebook' /></a>
+            <a href='https://www.facebook.com/palavrasdepaz.br' target="_blank"><Image id='face' onMouseOver={Evento4} src="/static/images/facebook.png" width="40px" height="40px" alt='facebook' /></a>
           </div>
           <div>
-          <a  href='https://wa.me/5511999756554' target="_blank"  ><Image  id='whats' onMouseOver={Evento5} src="/static/images/whatsapp.png" width="40px" height="40px" alt='whatsapp' /></a>
+            <a href='https://wa.me/5511999756554' target="_blank"  ><Image id='whats' onMouseOver={Evento5} src="/static/images/whatsapp.png" width="40px" height="40px" alt='whatsapp' /></a>
           </div>
         </div>
         <div className='line-footer'></div>
@@ -35,45 +36,45 @@ function Footer() {
           <p>ACESSO RÁPIDO</p>
         </div>
         <container className="opcoes-footer">
+
           <div className='colunas-footer'>
-            <h6>Quem Somos</h6>
-            <a href='/equipe'>Nossa equipe</a>
-            <a href='/quemsomos'>Nossa História</a>
+          <h6>Quem Somos</h6>
+            <Link href='/equipe'>Nossa equipe</Link>
+            <Link href='/quemsomos'>Nossa História</Link>
 
           </div>
 
           <div className='colunas-footer'>
             <h6>O Programa</h6>
-            <a>Reunião</a>
-            <a href='https://www.premrawat.com/pt/' target="_blank">Prew Rawart</a>
-            <a href='https://tprf.org/pt-br/' target="_blank">TPRF</a>
+            <Link href="#">Reunião</Link>
+            <Link href='https://www.premrawat.com/pt/' target="_blank">Prew Rawart</Link>
+            <Link href='https://tprf.org/pt-br/' target="_blank">TPRF</Link>
           </div>
 
           <div className='colunas-footer'>
             <h6>Voluntários</h6>
-            <a>Cadastro</a>
-            <a href='https://www.atados.com.br/' target="_blank">Atados</a>
+            <Link href="#">Cadastro</Link>
+            <Link href='https://www.atados.com.br/' target="_blank">Atados</Link>
           </div>
 
           <div className='colunas-footer'>
             <h6>Parcerias</h6>
-            <a href='/parcerias'>Objetivo</a>
-            <a>Contato</a>
-          </div>
+            <Link href='/parcerias'>Objetivo</Link>
+            <Link href="#">Contato</Link>
+             </div>
 
           <div className='colunas-footer'>
             <h6>Doações</h6>
-           <a>Prestação de contas</a>
-           <a href='/doacoes'>PIX</a>
-           <a href='doacoes'>Pay Pal</a>
-          </div>
+            <Link href="#">Prestação de contas</Link>
+            <Link href='/doacoes'>PIX</Link>
+            <Link href='doacoes'>Pay Pal</Link>
+             </div>
 
           <div className='colunas-footer'>
             <h6>Home</h6>
-           <a href='/galeria'>Galeria de fotos</a>
-           <a href='/depoimentos'>Depoimentos</a>
-
-          </div>
+            <Link href='/galeria'>Galeria de fotos</Link>
+            <Link href='/depoimentos'>Depoimentos</Link>
+            </div>
 
         </container>
         <container className="rodape-footer">
