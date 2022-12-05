0c906343b3c78c4f4159a8963b7f018ff32632f8
EnioSFilho <eniofilho76@gmail.com>
2022-12-04 22:50:54 -0300

    feat/devs em construção

---
+++ src/components/desenvolvedores/Devs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Devs() {
+  return (
+    <>
+    <h1>Teste</h1>
+
+    </>
+  )
+}
+
+export default Devs
---
+++ src/templates/devs/devs.template.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import FixedButton from '../../components/fixedbutton/FixedButton'
+import Footer from '../../components/footer/Footer'
+import Header from '../../components/header/Header'
+
+function DevsTemplate() {
+  return (
+   <>
+   <Home/>
+
+   <FixedButton/>
+   <Footer/>
+
+   </>
+  )
+}
+
+export default DevsTemplate
