c168bd85a63f8752d1df0d2ec863640c9d1a6fb0
EnioSFilho <eniofilho76@gmail.com>
2022-12-05 14:15:48 -0300

    footer refatorado - faltam apenas alguns links, preciso ver com a Amanda, mas pode mergear por favor

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
+import Evento1 from "./bntYou"
+import Evento2 from "./bntInsta"
+import Evento3 from "./btnLink"
+import Evento4 from "./btnFace"
+import Evento5 from './bntWhats'
 
 
 
@@ -10,19 +15,19 @@ function Footer() {
         <div className='line-footer'></div>
         <div className='medias-footer'>
           <div>
-            <Image src="/static/images/youtube.png" width="40px" height="40px" alt='' />
+            <a href='https://www.youtube.com/channel/UCkmhiQTeUfdvKIY9NG9Navg' target="_blank"><Image id="you"onMouseOver={Evento1} src="/static/images/youtube.png" width="40px" height="40px" alt='youtube' /></a>
           </div>
           <div>
-            <Image src="/static/images/instagram.png" width="40px" height="40px" alt='' />
+            <a href='https://www.instagram.com/palavrasdepaz.br/' target="_blank"><Image id="insta"onMouseOver={Evento2} src="/static/images/instagram.png" width="40px" height="40px" alt='instagram' /></a>
           </div>
           <div>
-            <Image src="/static/images/linkedin.png" width="40px" height="40px" alt='' />
+           <a href='https://www.linkedin.com/company/palavrasdepaz/mycompany/' target="_blank"> <Image id='link' onMouseOver={Evento3} src="/static/images/linkedin.png" width="40px" height="40px" alt='linkedin' /></a>
           </div>
           <div>
-            <Image src="/static/images/facebook.png" width="40px" height="40px" alt='' />
+          <a href='https://www.facebook.com/palavrasdepaz.br' target="_blank"><Image id='face' onMouseOver={Evento4} src="/static/images/facebook.png" width="40px" height="40px" alt='facebook' /></a>
           </div>
           <div>
-            <Image src="/static/images/whatsapp.png" width="40px" height="40px" alt='' />
+          <a  href='https://wa.me/5511999756554' target="_blank"  ><Image  id='whats' onMouseOver={Evento5} src="/static/images/whatsapp.png" width="40px" height="40px" alt='whatsapp' /></a>
           </div>
         </div>
         <div className='line-footer'></div>
@@ -32,35 +37,44 @@ function Footer() {
         <container className="opcoes-footer">
           <div className='colunas-footer'>
             <h6>Quem Somos</h6>
-            <p>Lorem</p>
-            <p>Lorem</p>
-            <p>Lorem</p>
+            <a href='/equipe'>Nossa equipe</a>
+            <a href='/quemsomos'>Nossa História</a>
+
           </div>
 
           <div className='colunas-footer'>
             <h6>O Programa</h6>
-            <p>Lorem</p>
-            <p>Lorem</p>
-            <p>Lorem</p>
+            <a>Reunião</a>
+            <a href='https://www.premrawat.com/pt/' target="_blank">Prew Rawart</a>
+            <a href='https://tprf.org/pt-br/' target="_blank">TPRF</a>
           </div>
 
           <div className='colunas-footer'>
             <h6>Voluntários</h6>
-            <p>Lorem</p>
-          </div>
-
-          <div className='colunas-footer'>
-            <h6>Doações</h6>
-            <p>Lorem</p>
-            <p>Lorem</p>
-            <p>Lorem</p>
+            <a>Cadastro</a>
+            <a href='https://www.atados.com.br/' target="_blank">Atados</a>
           </div>
 
           <div className='colunas-footer'>
             <h6>Parcerias</h6>
-            <p>Lorem</p>
-            <p>Lorem</p>
+            <a href='/parcerias'>Objetivo</a>
+            <a>Contato</a>
           </div>
+
+          <div className='colunas-footer'>
+            <h6>Doações</h6>
+           <a>Prestação de contas</a>
+           <a href='/doacoes'>PIX</a>
+           <a href='doacoes'>Pay Pal</a>
+          </div>
+
+          <div className='colunas-footer'>
+            <h6>Home</h6>
+           <a href='/galeria'>Galeria de fotos</a>
+           <a href='/depoimentos'>Depoimentos</a>
+
+          </div>
+
         </container>
         <container className="rodape-footer">
           <div>
